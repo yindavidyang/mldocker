087f922c59380b313e1433bd9cea0804ef142b49
David <yin@yang.net>
2020-12-12 23:41:57 +0300

    simplify magma recipe

--- conda_recipes/jetson/magma/build.sh
+++ conda_recipes/jetson/magma/build.sh
@@ -1,5 +1,24 @@
-mkdir build
-cd build
-cmake ..
-make -j $CPU_COUNT
-prefix=$PREFIX make install
+# export CUDADIR=/usr/local/cuda
+# cp -f make.inc-examples/make.inc.openblas make.inc
+# sed -i 's/^#GPU_TARGET ?= \([[:alnum:][:blank:]]\+\)/GPU_TARGET ?= VOLTA/' make.inc
+# sed -i 's/^NVCC\s*= nvcc/NVCC = \/usr\/local\/cuda\/bin\/nvcc/' make.inc
+# sed -i 's/^#OPENBLASDIR ?= \/usr\/local\/openblas/OPENBLASDIR ?= \($PREFIX\)/' make.inc
+# cat >> make.inc <<EOF
+# CFLAGS += -D_FORCE_INLINES
+# CXXFLAGS += -D_FORCE_INLINES
+# NVCCFLAGS += -D_FORCE_INLINES
+# EOF
+
+# export OPENBLASDIR=$CONDA_PREFIX
+# make -j $CPU_COUNT
+# prefix=$PREFIX make install
+
+
+# mkdir build
+# cd build
+# cmake ..
+# make -j $CPU_COUNT
+# make install
+cp /usr/local/lib/libmagma* $PREFIX/lib/
+cp /usr/local/include/magma* $PREFIX/include/
+cp -r /usr/local/lib/pkgconfig/ $PREFIX/lib/
